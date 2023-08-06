055e15904a9674b8a59d5248213a8dcc381d0528
ataberkuysal <atabrk.uysl@gmail.com>
2023-08-06 17:07:44 +0300

    Chapter2 - Validation.

--- src/main/java/com/ata/tacocloud/tacos/web/DesignTacoController.java
+++ src/main/java/com/ata/tacocloud/tacos/web/DesignTacoController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.ata.tacocloud.tacos.Taco;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 import com.ata.tacocloud.tacos.Ingredient;
@@ -58,7 +60,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors,
+                              @ModelAttribute TacoOrder tacoOrder) {
+
+        if(errors.hasErrors()){
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
--- src/main/java/com/ata/tacocloud/tacos/web/OrderController.java
+++ src/main/java/com/ata/tacocloud/tacos/web/OrderController.java
@@ -1,6 +1,7 @@
 package com.ata.tacocloud.tacos.web;
 
 import com.ata.tacocloud.tacos.TacoOrder;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,19 +17,13 @@ import java.util.ArrayList;
 @SessionAttributes("tacoOrder")
 public class OrderController {
     @GetMapping("/current")
-    public String orderForm(@ModelAttribute TacoOrder tacoOrder, Model model) {
-        /*model.addAttribute("tacoOrder", new TacoOrder());*/
+    public String orderForm() {
         return "orderForm";
     }
-    /*
-    @ModelAttribute
-    public TacoOrder getTacoOrder(){
-        return new TacoOrder();
-    }
-    */
+
     @PostMapping
-    public String processOrder(TacoOrder order,
-                               SessionStatus sessionStatus, Errors errors) {
+    public String processOrder(@Valid TacoOrder order, Errors errors,
+                               SessionStatus sessionStatus) {
         if(errors.hasErrors()){
             return "orderForm";
         }
